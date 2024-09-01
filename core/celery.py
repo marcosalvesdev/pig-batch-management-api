a7157cf8f4bb6ff53bb20bb693046ccf8cf44858
marcosalves.dev <marcosalves.dev@gmail.com>
2024-09-01 19:50:31 -0300

    Update quotation mark

--- core/celery.py
+++ core/celery.py
@@ -2,15 +2,15 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-app = Celery('pig_batch_management_api')
+app = Celery("pig_batch_management_api")
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
 
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f"Request: {self.request!r}")
